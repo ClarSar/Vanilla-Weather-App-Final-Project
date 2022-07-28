03447b29e9d9cca717849fe5fba434c7a90ab2bd
Sarah Clarke <107431263+ClarSar@users.noreply.github.com>
2022-07-28 12:48:27 +0100

    Add API to get daily forecast using JS

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -33,7 +34,7 @@ function displayForecast() {
     <div class="col-2">
         <div class="weather-forecast-date">${day}</div>
         <img
-          src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+          src="http://openweathermap.org/img/wn/10d@2x.png"
           alt=""
           width="42"
         />
@@ -47,6 +48,14 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "749d11da7cc4bf5dcb36a5fdf40ecee1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -71,6 +80,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -113,5 +124,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-displayForecast();
 search("Galway");
